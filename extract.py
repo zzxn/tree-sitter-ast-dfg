dfe007241aa4869d2810edd22bc95f153e060676
zzxn <zhuxn@shihe.tech>
2022-06-21 16:48:36 +0800

    fix import to make it portable

--- extract.py
+++ extract.py
@@ -1,5 +1,5 @@
-from parser import DFG_go, DFG_java, DFG_javascript, DFG_php, DFG_python, DFG_ruby
-from parser import (
+from .parser import DFG_go, DFG_java, DFG_javascript, DFG_php, DFG_python, DFG_ruby
+from .parser import (
     index_to_code_token,
     remove_comments_and_docstrings,
     tree_to_token_index,
